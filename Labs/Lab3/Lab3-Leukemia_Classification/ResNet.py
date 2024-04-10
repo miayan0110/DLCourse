f5d58c3aa50051f8fc21333a102dd85b27046828
mia_yen <mia_yen@gss.com.tw>
2024-04-10 18:23:27 +0800

    Lab 3 完成ResNet架構、main-train開工

--- Labs/Lab3/Lab3-Leukemia_Classification/ResNet.py
+++ Labs/Lab3/Lab3-Leukemia_Classification/ResNet.py
@@ -4,20 +4,20 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 class ResBlock(nn.Module):
-    def __init__(self, in_c, out_c, kernel_size, stride, downsample=False):
+    def __init__(self, in_c, out_c, kernel_size, stride, is_downsample=False):
         super(ResBlock, self).__init__()
-        
+        self.is_downsample = is_downsample
+
         self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=kernel_size, stride=stride)
         self.bn1 = nn.BatchNorm2d(out_c)
         self.relu = nn.ReLU()
         self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=kernel_size)
         self.bn2 = nn.BatchNorm2d(out_c)
-        if downsample:
+        if is_downsample:
             self.downsample = nn.Sequential(
                 nn.Conv2d(in_c, out_c, kernel_size=1, stride=2),
                 nn.BatchNorm2d(out_c)
             )
-            
 
     def forward(self, x):
         sample = x
@@ -28,15 +28,16 @@ class ResBlock(nn.Module):
         output = self.conv2(output)
         output = self.bn2(output)
 
-        if self.downsample != None:
+        if self.is_downsample:
             sample = self.downsample(x)
 
         output += sample
         return nn.ReLU(output)
 
 class ResBottleneck(nn.Module):
-    def __init__(self, previous_out, in_c, out_c, kernel_size, stride, downsample=False):
+    def __init__(self, previous_out, in_c, out_c, kernel_size, stride, is_downsample=False):
         super(ResBottleneck, self).__init__()
+        self.is_downsample = is_downsample
 
         self.conv1 = nn.Conv2d(previous_out, in_c, kernel_size=1, stride=stride)
         self.bn1 = nn.BatchNorm2d(in_c)
@@ -46,12 +47,11 @@ class ResBottleneck(nn.Module):
         self.relu2 = nn.ReLU()
         self.conv3 = nn.Conv2d(in_c, out_c, kernel_size=1)
         self.bn3 = nn.BatchNorm2d(out_c)
-        if downsample:
+        if is_downsample:
             self.downsample = nn.Sequential(
                 nn.Conv2d(previous_out, out_c, kernel_size=1, stride=2),
                 nn.BatchNorm2d(out_c)
             )
-            
 
     def forward(self, x):
         sample = x
@@ -65,43 +65,36 @@ class ResBottleneck(nn.Module):
         output = self.conv3(output)
         output = self.bn3(output)
 
-        if self.downsample != None:
+        if self.is_downsample:
             sample = self.downsample(x)
 
         output += sample
         return nn.ReLU(output)
 
 
-
 class ResNet(nn.Module):
-    def __init__(self, in_channels, block_def):
+    def __init__(self, mode, block, in_channels, block_def):
         super(ResNet, self).__init__()
-        self.out1 = 64
+        self.mode = mode
+        self._in = 64
+        self._out = 64
+        self.previous_out = self._out
 
         self.conv1 = nn.Sequential(
-            nn.Conv2d(in_channels, self.out1, kernel_size=7, stride=2),
-            nn.BatchNorm2d(self.out1),
+            nn.Conv2d(in_channels, self._out, kernel_size=7, stride=2),
+            nn.BatchNorm2d(self._out),
             nn.ReLU()
         )
 
         self.maxPool = nn.MaxPool2d(kernel_size=3, stride=2)
-
-
-class ResNet18(ResNet):
-    def __init__(self, mode, block, in_channels, block_def):
-        super(ResNet18, self).__init__(in_channels, block_def)
-        self.mode = mode
-        self._in = self.out1
-        self._out = self.out1
-        self.previous_out = self.out1
 
         self.conv2x = self.generate_block(block, block_def[0], 3, 1)
         self.conv3x= self.generate_block(block, block_def[1], 3, 2, True)
         self.conv4x = self.generate_block(block, block_def[2], 3, 2, True)
         self.conv5x = self.generate_block(block, block_def[3], 3, 2, True)
         self.avgPool = nn.AvgPool2d(kernel_size=7)
-        
-        self.fc = nn.Linear(self._in, 2)
+
+        self.fc = nn.Linear(self.previous_out, 1)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -116,31 +109,32 @@ class ResNet18(ResNet):
         x = torch.flatten(x, 1)
         x = self.fc(x)
 
-
     def generate_block(self, block, layers, kernel_size=1, stride=1, downsample=False):
         layer = []
         is_downsample = downsample
         stride = stride
 
-        if self.mode == "18":
-            for _ in range(layers):
-                layer.append(block(self._in, self._out, kernel_size, stride, is_downsample))
-                self._in = self._out
-                if is_downsample:
-                    is_downsample = False
-                    stride = 1
-            
-            self._out *= 2
-        else:
-            self._out = self._in*4
-            for _ in range(layers):
-                layer.append(block(self.previous_out, self._in, self._out, kernel_size, stride, is_downsample))
-                self.previous_out = self._out
-                
-                if is_downsample:
-                    is_downsample = False
-                    stride = 1
-            
-            self._in *= 2
+        match self.mode:
+            case "18" | "34":
+                for _ in range(layers):
+                    layer.append(block(self._in, self._out, kernel_size, stride, is_downsample))
+                    self._in = self._out
+                    self.previous_out = self._out
+                    if is_downsample:
+                        is_downsample = False
+                        stride = 1
+
+                self._out *= 2
+            case _:
+                self._out = self._in*4
+                for _ in range(layers):
+                    layer.append(block(self.previous_out, self._in, self._out, kernel_size, stride, is_downsample))
+                    self.previous_out = self._out
+
+                    if is_downsample:
+                        is_downsample = False
+                        stride = 1
+
+                self._in *= 2
 
         return nn.Sequential(*layer)
--- Labs/Lab3/Lab3-Leukemia_Classification/main.py
+++ Labs/Lab3/Lab3-Leukemia_Classification/main.py
@@ -1,6 +1,20 @@
 import pandas as pd
 import ResNet.py as rn
-import dataloader
+import dataloader.py as dl
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# hyperparameters
+hps = {
+    "learning_rate": 0.01,
+    "batch_size": 10,
+    "n_epochs": 2
+}
+
+# history
+history = {"train_loss": [], "train_acc": [], "test_loss": [], "test_acc": []}
+
 
 def evaluate():
     print("evaluate() not defined")
@@ -8,7 +22,46 @@ def evaluate():
 def test():
     print("test() not defined")
 
-def train():
+def train(dataLoader, model):
+    dataset_size = dataLoader.__len__()
+    n_epochs = hps["n_epochs"]
+    n_batches = dataset_size//hps["batch_size"] + (dataset_size%hps["batch_size"] > 0)
+    optimizer = optim.Adam(model.parameters(), lr=hps["learning_rate"])
+
+    for epoch in range(n_epochs):
+        features = []
+        labels = []
+        correct = 0.0
+        cost = 0.0
+
+        for batch in range(n_batches):
+
+            for i in range(hps["batch_size"]):
+                feature, label = dataLoader.__getitem__(i+epoch*hps["batch_size"])
+                features.append(feature)
+                labels.append(label)
+
+            model.train()
+
+            optimizer.zero_grad()
+
+            pred = model(torch.FloatTensor(features))
+            loss = nn.CrossEntropyLoss(pred, torch.FloatTensor(labels).long())
+            loss.backward()
+
+            optimizer.step()
+
+            correct += (pred == labels).sum()
+            cost += loss.item()
+        epoch_acc = correct/dataset_size
+        epoch_loss = cost/n_batches
+        history["train_acc"].append(epoch_acc)
+        history["train_loss"].append(epoch_loss)
+
+        print(f"[ Train | {epoch:3d}/{n_epochs:3d} ] loss = {epoch_loss:.5f}, acc = {epoch_acc:.5f}")
+
+
+        
     print("train() not defined")
 
 def save_result(csv_path, predict_result):
@@ -20,5 +73,7 @@ def save_result(csv_path, predict_result):
 
 if __name__ == "__main__":
     print("Good Luck :)")
-    res18 = rn.ResNet18([2, 2, 2, 2])
-    res18.train()
+    dataLoader = dl.LeukemiaLoader("./training_data/", "train")
+
+    res18 = rn.ResNet("18", rn.ResBlock, 3, [2, 2, 2, 2])
+    train(dataLoader, res18)
