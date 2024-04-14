b2194751bb0b74019c1ecf59a6ea5af844282a27
miayan0110 <miayan0110@gmail.com>
2024-04-14 23:33:39 +0800

    Lab 3 數據紀錄

--- Labs/Lab3/Lab3-Leukemia_Classification/main.py
+++ Labs/Lab3/Lab3-Leukemia_Classification/main.py
@@ -7,10 +7,7 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 from tqdm import tqdm
-
-
-# history
-history = {"train_loss": [], "train_acc": [], "test_loss": [], "test_acc": []}
+import gc
 
 
 def evaluate(valid_loader, model):
@@ -21,22 +18,22 @@ def evaluate(valid_loader, model):
     batch_valid_correct = []
 
     model.eval()
+    with torch.no_grad():
+        for batch in tqdm(valid_loader):
+            features, labels = batch
+            features = features.float().to(device)
+            labels = labels.long().to(device)
 
-    for batch in tqdm(valid_loader):
-        features, labels = batch
-        features = features.float().to(device)
-        labels = labels.long().to(device)
+            pred = model(features)
+            loss = crossEntropy(pred, labels)
 
-        pred = model(features)
-        loss = crossEntropy(pred, labels)
+            batch_valid_cost.append(loss.item())
+            batch_valid_correct.append((pred.argmax(dim=1) == labels).float().mean())
 
-        batch_valid_cost.append(loss.item())
-        batch_valid_correct.append((pred.argmax(dim=1) == labels).float().mean())
+        valid_loss = sum(batch_valid_cost)/len(batch_valid_cost)
+        valid_acc = sum(batch_valid_correct)/len(batch_valid_correct)
 
-    valid_loss = sum(batch_valid_cost)/len(batch_valid_cost)
-    valid_acc = sum(batch_valid_correct)/len(batch_valid_correct)
-
-    print(f"validation loss = {valid_loss:5f}, validation acc = {valid_acc:.5f}")
+        print(f"validation loss = {valid_loss:5f}, validation acc = {valid_acc:.5f}")
 
 
 def test(test_loader, model, max_acc, min_loss):
@@ -47,31 +44,31 @@ def test(test_loader, model, max_acc, min_loss):
     batch_test_correct = []
 
     model.eval()
+    with torch.no_grad():
+        for batch in tqdm(test_loader):
+            features, labels = batch
+            features = features.float().to(device)
+            labels = labels.long().to(device)
 
-    for batch in tqdm(test_loader):
-        features, labels = batch
-        features = features.float().to(device)
-        labels = labels.long().to(device)
+            pred = model(features)
+            loss = crossEntropy(pred, labels)
 
-        pred = model(features)
-        loss = crossEntropy(pred, labels)
+            batch_test_cost.append(loss.item())
+            batch_test_correct.append((pred.argmax(dim=1) == labels).float().mean())
 
-        batch_test_cost.append(loss.item())
-        batch_test_correct.append((pred.argmax(dim=1) == labels).float().mean())
+        test_loss = sum(batch_test_cost)/len(batch_test_cost)
+        test_acc = sum(batch_test_correct)/len(batch_test_correct)
 
-    test_loss = sum(batch_test_cost)/len(batch_test_cost)
-    test_acc = sum(batch_test_correct)/len(batch_test_correct)
+        history[hps["model"]+"_test_loss"].append(test_loss)
+        history[hps["model"]+"_test_acc"].append(test_acc.item())
 
-    history["test_loss"].append(test_loss)
-    history["test_acc"].append(test_acc)
+        print(f"test loss = {test_loss:5f}, test acc = {test_acc:.5f}")
 
-    print(f"test loss = {test_loss:5f}, test acc = {test_acc:.5f}")
-
-    if test_acc >= max_acc and test_loss <= min_loss: 
-        save_model(model)
-        return test_acc, test_loss
-    else:
-        return max_acc, min_loss
+        if test_acc >= max_acc and test_loss <= min_loss: 
+            save_model(model)
+            return test_acc, test_loss
+        else:
+            return max_acc, min_loss
 
 
 def train(train_loader, test_loader, model):
@@ -105,8 +102,8 @@ def train(train_loader, test_loader, model):
 
         epoch_loss = sum(batch_train_cost)/len(batch_train_cost)
         epoch_acc = sum(batch_train_correct)/len(batch_train_correct)
-        history["train_acc"].append(epoch_acc)
-        history["train_loss"].append(epoch_loss)
+        history[hps["model"]+"_train_acc"].append(epoch_acc.item())
+        history[hps["model"]+"_train_loss"].append(epoch_loss)
 
         print(f"[ Train | {epoch+1:02d}/{n_epochs:02d} ] loss = {epoch_loss:.5f}, acc = {epoch_acc:.5f}")
 
@@ -125,8 +122,12 @@ def plot(data, n_epochs):
     plt.xlabel("Epoch")
     plt.ylabel("Accuracy(%)")
     
-    plt.plot(epochs, data["train_acc"], label="ResNet18 Train")
-    plt.plot(epochs, data["test_acc"], label="ResNet18 Test")
+    plt.plot(epochs, data["18_train_acc"], label="ResNet18 Train")
+    plt.plot(epochs, data["18_test_acc"], label="ResNet18 Test")
+    plt.plot(epochs, data["50_train_acc"], label="ResNet50 Train")
+    plt.plot(epochs, data["50_test_acc"], label="ResNet50 Test")
+    plt.plot(epochs, data["152_train_acc"], label="ResNet152 Train")
+    plt.plot(epochs, data["152_test_acc"], label="ResNet152 Test")
 
     plt.legend()
     plt.show()
@@ -139,6 +140,10 @@ def save_result(csv_path, predict_result):
     new_df["label"] = predict_result
     new_df.to_csv("./your_student_id_resnet18.csv", index=False)
 
+# history
+history = {"18_train_loss": [], "18_train_acc": [], "18_test_loss": [], "18_test_acc": [],
+          "50_train_loss": [], "50_train_acc": [], "50_test_loss": [], "50_test_acc": [],
+          "152_train_loss": [], "152_train_acc": [], "152_test_loss": [], "152_test_acc": []}
 
 
 if __name__ == "__main__":
@@ -147,10 +152,10 @@ if __name__ == "__main__":
 
     # hyperparameters
     hps = {
-        "learning_rate": 0.001,
-        "batch_size": 128,
+        "learning_rate": 0.01,
+        "batch_size": 128,  # 8/4
         "n_epochs": 10,
-        "model": "18",
+        "model": "18",  #"50"/"152"
         "device": device,
         "data_path": "./training_data/",
         "save_path": "./models/new_res"
@@ -160,6 +165,8 @@ if __name__ == "__main__":
     test_loader = DataLoader(dl.LeukemiaLoader(hps["data_path"], "valid"), batch_size=hps["batch_size"], shuffle=False)
 
     res = rn.ResNet(hps["model"], rn.ResBlock, 3, [2, 2, 2, 2]).to(device)
+
+    gc.collect()
     # print(res)
     train(train_loader, test_loader, res)
     plot(history, hps["n_epochs"])
